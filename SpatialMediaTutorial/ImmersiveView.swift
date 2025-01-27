63c5eb92904182bd97ad865fcdae36d30ab60d2d
Riccardoios <riccardo.rich@me.com>
2025-01-27 14:50:14 +0100

    add scaling

--- SpatialMediaTutorial/ImmersiveView.swift
+++ SpatialMediaTutorial/ImmersiveView.swift
@@ -12,38 +12,60 @@ import SwiftUI
 struct ImmersiveView: View {
     @State var spatialVideoState: SpatialVideoState
     @State var spatialGalleryState: SpatialGalleryState
+    @State var isPhotoBig: Bool = false
 
     var body: some View {
         RealityView { content, attachments in
             if let immersiveContentEntity = try? await Entity(
-                named: "AAA_MainScene", in: studioBundle) {
+                named: "AAA_MainScene", in: studioBundle)
+            {
                 content.add(immersiveContentEntity)
             }
-            
+
             let videoEntity = spatialVideoState.makeSpatialVideo()
             videoEntity.position = [0.5, 1.5, -1.5]
             content.add(videoEntity)
-            
+
             if let spatialPhoto = attachments.entity(for: "SpatialPhoto") {
                 spatialPhoto.position = [-0.5, 1.5, -1.5]
                 content.add(spatialPhoto)
             }
             
+            if let photoControls = attachments.entity(for: "PhotoControls") {
+                photoControls.position = [-0.5, 1.3, -1.5]
+                content.add(photoControls)
+            }
+
             if let videoControls = attachments.entity(for: "Video Controls") {
                 videoControls.position = [0.5, 1.3, -1.5]
                 content.add(videoControls)
             }
+
+        } update: { content, attachments in
+            
+            if let spatialPhoto = attachments.entity(for: "SpatialPhoto") {
+                spatialPhoto.position = [-0.5, 1.5, -1.5]
+                let scaling: Float = isPhotoBig ? 3.0 : 1.0
+                spatialPhoto.scale *= scaling
+            }
+            
         } attachments: {
             Attachment(id: "SpatialPhoto") {
                 SpatialGalleryView(state: spatialGalleryState)
             }
-            
+
+            Attachment(id: "PhotoControls") {
+                Button("scale photo") {
+                    isPhotoBig.toggle()
+                }
+            }
+
             Attachment(id: "Video Controls") {
                 HStack {
                     Button("Play") {
                         spatialVideoState.play()
                     }
-                    
+
                     Button("Pause") {
                         spatialVideoState.pause()
                     }
